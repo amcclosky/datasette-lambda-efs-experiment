321a933c8ca89f7896aa60e9c0c653bd166ad84e
amcclosky <anthony@mcclosky.dev>
2020-07-22 06:03:57 +0000

    working. lambda + efs + datasette

--- datasette-test/datasette_lambda/app.py
+++ datasette-test/datasette_lambda/app.py
@@ -1,31 +1,80 @@
 import json
-from pathlib import Path
+import os
 
-# You can reference EFS files by including your local mount path, and then
-# treat them like any other file. Local invokes may not work with this, however,
-# as the file/folders may not be present in the container.
+from mangum import Mangum
+from datasette.app import Datasette, DEFAULT_CONFIG
+from datasette.utils import value_as_boolean
 
-FILE = Path("/mnt/lambda/file")
+METADATA_PATH = '/var/task/metadata.json'
+CONFIG_PATH = '/var/task/config.txt'
+METADATA_PATH = '/var/task/metadata.json'
+PREFIX = 'Prod'
+
+def load_metadata():
+    """Load the metadata.json file, if present."""
+    metadata = {}
+    if os.path.exists(METADATA_PATH):
+        metadata = json.loads(open(METADATA_PATH).read())
+
+    return metadata
+
+def load_config():
+    """Load and parse config settings, if present."""
+    config = {}
+    if os.path.exists(CONFIG_PATH):
+        for line in open(CONFIG_PATH):
+            if not line:
+                continue
+
+            line = line.strip()
+
+            if ':' not in line:
+                raise Exception('"{}" should be name:value'.format(line))
+
+            key = line[0:line.find(':')]
+            value = line[line.find(':') + 1:]
+
+            # This is a hack; properly we should be introspecting the annotations in cli.py.
+            # Still, this works for many of the common settings, so *shrug*.
+            if key not in DEFAULT_CONFIG:
+                raise Exception('unknown config setting: ' + key)
+
+            default = DEFAULT_CONFIG[key]
+            if isinstance(default, bool):
+                value = value_as_boolean(value)
+            elif isinstance(default, int):
+                value = int(value)
+
+            config[key] = value
+            
+    if PREFIX:
+        config['base_url'] = '/{}/'.format(PREFIX)
+    return config
+
+
+def create_handler():
+    """Create the Datasette ASGI handler and wrap it in the Mangum adapter."""
+    datasette = Datasette(
+        files=["/var/task/test.db"],
+        immutables=[],
+        cache_headers=True,
+        cors=True,
+        inspect_data=None,
+        metadata=load_metadata(),
+        sqlite_extensions=None, #sqlite_extensions,
+        template_dir=None, #template_dir,
+        plugins_dir=None, #plugins_dir,
+        static_mounts=None, #static,
+        config=load_config(),
+        memory=False, #memory,
+        version_note=None #version_note,
+    )
+    app = datasette.app()
+    return Mangum(app)
+
+handler_ = create_handler()
 
 def lambda_handler(event, context):
-    wrote_file = False
-
-    contents = None
-    # The files in EFS are not only persistent across executions, but if multiple
-    # Lambda functions are mounted to the same EFS file system, you can read and
-    # write files from either function.
-    if not FILE.is_file():
-        with open(FILE, 'w') as f:
-            contents = "Hello, EFS!\n"
-            f.write(contents)
-            wrote_file = True
-    else:
-        with open(FILE, 'r') as f:
-            contents = f.read()
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "file_contents": contents,
-            "created_file": wrote_file
-        }),
-    }
+    print(f"event: {event}")
+    print(f"context: {context}")
+    return handler_(event, context)
